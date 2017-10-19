22ea9b55aa7b8ed229824eda3cb105208e843527
ClausClaus <weixinla@foxmail.com>
2017-10-19 10:44:55 +0800

    设置路由懒加载，减少文件大小。更新vue等各个核心依赖的版本

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import fastclick from 'fastclick'
 import VuelazyLoad from 'vue-lazyload';
 import store from './store'; // 引入vuex
+// import VConsole from 'vconsole'; // 在移动端实现控制台输出
 fastclick.attach(document.body); // 解决移动端点击延迟300毫秒
 import 'common/stylus/index.styl'; // 引入公共的样式文件
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,60 @@
  */
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
-import SingerDetail from 'components/singer-detail/singer-detail';
-import Disc from 'components/disc/disc.vue';
-import TopList from 'components/top-list/top-list.vue';
-import UserCenter from 'components/user-center/user-center.vue';
+// import Recommend from 'components/recommend/recommend'
+// import Singer from 'components/singer/singer'
+// import Rank from 'components/rank/rank'
+// import Search from 'components/search/search'
+// import SingerDetail from 'components/singer-detail/singer-detail';
+// import Disc from 'components/disc/disc';
+// import TopList from 'components/top-list/top-list';
+// import UserCenter from 'components/user-center/user-center';
 
 Vue.use(Router)
+
+/* 异步加载路由组件 start */
+const Recommend = ((resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+})
+const Singer = ((resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+})
+const Rank = ((resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+})
+const Search = ((resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+})
+const SingerDetail = ((resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+})
+const Disc = ((resolve) => {
+  import('components/disc/disc').then((module) => {
+    resolve(module)
+  })
+})
+const TopList = ((resolve) => {
+  import('components/top-list/top-list').then((module) => {
+    resolve(module)
+  })
+})
+const UserCenter = ((resolve) => {
+  import('components/user-center/user-center').then((module) => {
+    resolve(module)
+  })
+})
+/* 异步加载路由组件 end */
+
 
 export default new Router({
   routes: [
